82f773fb104148d529575d5d9402d468e920ae93
hannahoeidahl <hanhoi02118@stud.noroff.no>
2024-04-26 10:38:32 +0200

    Cloning repository

---
+++ scripts/index.mjs
@@ -0,0 +1,50 @@
+
+// Fetching API
+const API_BASE_URL = "https://api.noroff.dev/api/v1/square-eyes";
+
+async function getData() {
+  try {
+    const response = await fetch(API_BASE_URL);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  } 
+}
+
+getData();
+
+async function displayMovies() {
+  const data = await getData();
+  const mainContent = document.getElementById('main-content');
+  
+  // Loop through each product and create HTML elements to display them
+  data.forEach(product => {
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+
+    const img = document.createElement('img');
+    img.src = product.image;
+    img.alt = product.title;
+    productDiv.appendChild(img);
+
+    const title = document.createElement('h2');
+    title.textContent = product.title;
+    productDiv.appendChild(title);
+
+    const genre = document.createElement('p');
+    genre.textContent = `Genre: ${product.genre}`;
+    productDiv.appendChild(genre);
+
+    const price = document.createElement('p');
+    price.textContent = `Price: ${product.price}`;
+    productDiv.appendChild(price);
+
+    mainContent.appendChild(productDiv);
+  });
+}
+
+displayMovies();
+
+
+
---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Fetch API, og vis filmer på forsiden av nettside
+// Legg inn knapper så brukeren kan filtrere produktene på forsiden med kategorier
+// Lag en produktside og link filmene riktig fra API
+// Legg inn knapp så produktet kan bli lagt i handlekurven
+// Kode handlekurv slik at produktene kan fjernes fra handlekurv
+// Knapp på checkoutpage slik at produktene kan kjøpes, så bli sendt til confirmation page. 
+
+// Nødvendige sider: index.html, /product/index.html, checkout/index.html, checkout/confirmation/index.html
+
+//Test under for å hente
